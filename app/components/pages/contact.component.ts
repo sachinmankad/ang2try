268f666b794d2650d3b0f29e7c012f7ad25860ab
unknown <U43041@C900LTRV.ustr.com>
2017-03-30 21:10:03 +0530

    Initial Commit

---
+++ app/components/pages/contact.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'contact',
+    template: '<h1>This is contact page</h1>'
+})
+
+export class ContactComponent {
+
+}
+
+
+
+
+
+
+/*import { Component } from '@angular/core';
+
+@Component({
+  moduleId:module.id,
+  selector: 'about',
+  templateUrl: 'about.component.html'
+})
+export class AboutComponent {
+
+}
+*/
